858b65084983dc1be79813be5f0e3ab0322921d4
Tejas Chendekar <tejas@Tejas-MacBook.local>
2024-04-26 15:46:15 +0100

    Added changes

--- engine/meta/enviornment.py
+++ engine/meta/enviornment.py
@@ -1,6 +1,6 @@
 class Environment:
-    def __init__(self, service, load_balancer, meta_layer, time):
-        self.services = service
+    def __init__(self, services, load_balancer, meta_layer, time):
+        self.services = services
         self.load_balancer = load_balancer
         self.meta_layer = meta_layer
         self.time = time
